2204acf1a45bffab3d73a4224339b4dd9f3d7bbb
Matt Burke <spraints@gmail.com>
2025-07-04 12:12:48 -0400

    Start implementing 'status'

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let cli = Cli::parse();
     let config = Config::load(cli.config.as_deref()).unwrap();
     match &cli.command {
-        None | Some(Commands::Status) => cmd_status(config),
+        None | Some(Commands::Status) => cmd_status(&config),
         Some(Commands::Store(store_cmd)) => match &store_cmd.subcommand {
             StoreSubcommand::Create { git, path } => {
                 println!("Create store: git={}, path={}", git, path);
@@ -38,6 +38,31 @@ fn main() {
     }
 }
 
-fn cmd_status(config: Config) {
-    println!("{config:?}");
+fn cmd_status(config: &Config) {
+    let mut any_overrides = false;
+    if let Some(p) = &config.mindstorms_path {
+        println!("Mindstorms path override: {p:?}");
+        any_overrides = true;
+    }
+    if let Some(p) = &config.spike_path {
+        println!("Spike path override: {p:?}");
+        any_overrides = true;
+    }
+    if any_overrides {
+        println!();
+    }
+
+    if config.stores.len() == 0 {
+        println!("Get started by running '{} store create'.", exe());
+    }
+    for store in &config.stores {
+        println!("todo: show store state: {store:?}");
+        // I think I'm going to keep track of which files are
+    }
+}
+
+fn exe() -> String {
+    std::env::args()
+        .next()
+        .unwrap_or_else(|| "mind-meld".to_string())
 }
