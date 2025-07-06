eae0c958cf7b46db9bebc269ea5d5cd1817cbf39
Matt Burke <spraints@gmail.com>
2025-07-05 22:03:39 -0400

    Read a project from disk

--- src/project.rs
+++ src/project.rs
@@ -1,5 +1,10 @@
+use std::error::Error;
 use std::fmt::Display;
+use std::fs::File;
+use std::io::{Read, Seek};
 use std::path::PathBuf;
+
+use zip::ZipArchive;
 
 use crate::dirs::Dirs;
 
@@ -14,6 +19,22 @@ pub fn all_programs(dirs: Dirs) -> Vec<(Program, PathBuf)> {
         (Program::Mindstorms, dirs.mindstorms),
         (Program::Spike, dirs.spike),
     ]
+}
+
+pub fn read(id: &ProjectID, dirs: Dirs) -> Result<RawProject, Box<dyn Error>> {
+    let base_path = dir(id.program, dirs);
+    let path = base_path.join(&id.name);
+    let archive = ZipArchive::new(File::open(path)?)?;
+    Ok(RawProject {
+        archive: RawArchive::read(archive)?,
+    })
+}
+
+fn dir(prog: Program, dirs: Dirs) -> PathBuf {
+    match prog {
+        Program::Mindstorms => dirs.mindstorms,
+        Program::Spike => dirs.spike,
+    }
 }
 
 const PROGRAM_MINDSTORMS: &'static str = "mindstorms";
@@ -59,13 +80,39 @@ pub struct RawProject {
 pub struct RawArchive {
     pub entries: Vec<ArchiveEntry>,
 }
+impl RawArchive {
+    fn read<R: Read + Seek>(mut archive: ZipArchive<R>) -> Result<Self, Box<dyn Error>> {
+        let mut entries = Vec::new();
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i)?;
+            let mut buf = Vec::new();
+            file.read_to_end(&mut buf)?;
+            entries.push(ArchiveEntry::new(file.name(), buf)?);
+        }
+        Ok(Self { entries })
+    }
+}
 
 pub struct ArchiveEntry {
     pub name: String,
     pub contents: ArchiveEntryContents,
 }
 
+impl ArchiveEntry {
+    fn new(name: &str, buf: Vec<u8>) -> Result<Self, Box<dyn Error>> {
+        let contents = match name {
+            "scratch.sb3" => {
+                let archive = ZipArchive::new(std::io::Cursor::new(buf))?;
+                ArchiveEntryContents::Archive(RawArchive::read(archive)?)
+            }
+            _ => ArchiveEntryContents::Data(buf),
+        };
+        let name = name.to_string();
+        Ok(Self { name, contents })
+    }
+}
+
 pub enum ArchiveEntryContents {
     Data(Vec<u8>),
-    NestedZip(RawArchive),
+    Archive(RawArchive),
 }
